5e08a67ffde463052b2f452aa7ecd26a967c7c11
UtkarshPrakash <utkarshprakash21sep@gmail.com>
2025-01-02 21:47:07 +0530

    setting up simple express app

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const path = require("node:path");
+const router = require("./routes/routes");
+
+const app = express();
+const assetsPath = path.join(__dirname, "public");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/", router);
+
+PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on port: ${PORT}`);
+});
